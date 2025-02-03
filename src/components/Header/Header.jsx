4351414bfb31adb8e5dca0c69cadc6f530c31699
unknown <yavtushenkon@gmail.com>
2025-02-03 12:25:28 +0200

    navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,9 @@
-import { useLocation } from "react-router-dom";
 import Navigation from "../Navigation/Navigation";
 import s from "./header.module.css";
-
 const Header = () => {
-  const location = useLocation();
-
-  const getTitle = () => {
-    switch (location.pathname) {
-      case "/":
-        return null;
-      case "/contacts":
-        return "Contacts book";
-      default:
-        return "Welcome to our Contact Manager!";
-    }
-  };
-
   return (
-    <header className={s.header}>
-      {location.pathname === "/" ? null : (
-        <h1 className={s.title}>{getTitle()}</h1>
-      )}
+    <header>
+      <h1 className={s.title}>Contacts book</h1>
       <Navigation />
     </header>
   );
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,31 +1,15 @@
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { useAuth } from "../../redux/auth/operations";
 import { AuthNav } from "../AuthNav/AuthNav";
 import s from "./Navigation.module.css";
 
 const Navigation = () => {
   const { isLoggedIn } = useAuth();
-  const location = useLocation();
-
   return (
     <nav className={s.nav}>
-      {location.pathname === "/" ? (
-        <AuthNav />
-      ) : (
-        <>
-          <NavLink to="/" className={s.link}>
-            Home
-          </NavLink>
-          <NavLink to="/contacts" className={s.link}>
-            Contacts
-          </NavLink>
-          {isLoggedIn ? (
-            <button className={s.button}>Logout</button>
-          ) : (
-            <AuthNav />
-          )}
-        </>
-      )}
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/contacts">Contacts</NavLink>
+      {isLoggedIn ? <button className={s.button}>Logout</button> : <AuthNav />}
     </nav>
   );
 };
