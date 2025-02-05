dc0d59024d385b0e958041103f475c0c98b19c50
unknown <yavtushenkon@gmail.com>
2025-02-05 13:34:58 +0200

    logout

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -9,8 +9,12 @@ export const selectFilter = (state) => state.filters.name;
 export const selectContactsFilters = createSelector(
   [selectContacts, selectFilter],
   (contacts, filter) => {
+    if (!filter) {
+      return contacts;
+    }
+    const normalizedFilter = filter.toLowerCase();
     return contacts.filter((contact) =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
+      contact.name.toLowerCase().includes(normalizedFilter)
     );
   }
 );
